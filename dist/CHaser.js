f3209141552f8f71cb4ea61cdc4345d287f643a6
Ryoki Kamada <ryoki@asahikawa-tosei.jp>
2022-02-14 20:00:40 +0900

    0.5.6

---
+++ dist/CHaser.js
@@ -0,0 +1,476 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var process_1 = require("process");
+var promise_socket_1 = __importDefault(require("promise-socket"));
+var CHaser = /** @class */ (function () {
+    function CHaser() {
+        this.promiseSocket = new promise_socket_1.default();
+        this.response = "";
+        this.chunkSize_atMark = 3;
+        this.chunkSize_response = 12;
+        this.value = [1, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    }
+    CHaser.prototype.socket = function (host, port, name) {
+        return __awaiter(this, void 0, void 0, function () {
+            var e_1;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0:
+                        _a.trys.push([0, 3, , 4]);
+                        return [4 /*yield*/, this.promiseSocket.connect(port, host)];
+                    case 1:
+                        _a.sent();
+                        return [4 /*yield*/, this.promiseSocket.writeAll(name)];
+                    case 2:
+                        _a.sent();
+                        console.log("connect: socket host:" + host + " port:" + port + " name:" + name);
+                        return [3 /*break*/, 4];
+                    case 3:
+                        e_1 = _a.sent();
+                        console.error("error: socket : " + e_1);
+                        process_1.exit();
+                        return [3 /*break*/, 4];
+                    case 4: return [2 /*return*/];
+                }
+            });
+        });
+    };
+    CHaser.prototype.get_atMark = function () {
+        return __awaiter(this, void 0, void 0, function () {
+            var _a, e_2;
+            return __generator(this, function (_b) {
+                switch (_b.label) {
+                    case 0:
+                        _b.trys.push([0, 2, , 3]);
+                        _a = this;
+                        return [4 /*yield*/, this.promiseSocket.read(this.chunkSize_atMark)];
+                    case 1:
+                        _a.response = _b.sent();
+                        console.log("response: " + this.response.toString().substring(0, 1));
+                        return [3 /*break*/, 3];
+                    case 2:
+                        e_2 = _b.sent();
+                        console.error("error: get_atMark : " + e_2);
+                        process_1.exit();
+                        return [3 /*break*/, 3];
+                    case 3: return [2 /*return*/];
+                }
+            });
+        });
+    };
+    CHaser.prototype.get_ready = function () {
+        return __awaiter(this, void 0, void 0, function () {
+            var _a, e_3;
+            return __generator(this, function (_b) {
+                switch (_b.label) {
+                    case 0:
+                        _b.trys.push([0, 3, , 4]);
+                        return [4 /*yield*/, this.promiseSocket.writeAll("gr\r\n")];
+                    case 1:
+                        _b.sent();
+                        _a = this;
+                        return [4 /*yield*/, this.promiseSocket.read(this.chunkSize_response)];
+                    case 2:
+                        _a.response = _b.sent();
+                        console.log("response: get_ready : " + this.response.toString().substring(0, 10));
+                        this.check_first_str();
+                        this.make_value();
+                        return [3 /*break*/, 4];
+                    case 3:
+                        e_3 = _b.sent();
+                        console.error("error: get_ready : " + e_3);
+                        process_1.exit();
+                        return [3 /*break*/, 4];
+                    case 4: return [2 /*return*/];
+                }
+            });
+        });
+    };
+    CHaser.prototype.send_sharp = function () {
+        return __awaiter(this, void 0, void 0, function () {
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0: return [4 /*yield*/, this.promiseSocket.writeAll("#\r\n")];
+                    case 1:
+                        _a.sent();
+                        return [2 /*return*/];
+                }
+            });
+        });
+    };
+    CHaser.prototype.get_response = function () {
+        return __awaiter(this, void 0, void 0, function () {
+            var _a;
+            return __generator(this, function (_b) {
+                switch (_b.label) {
+                    case 0:
+                        _a = this;
+                        return [4 /*yield*/, this.promiseSocket.read(this.chunkSize_response)];
+                    case 1:
+                        _a.response = _b.sent();
+                        console.log("response: action : " + this.response.toString().substring(0, 10));
+                        this.check_first_str();
+                        this.make_value();
+                        return [2 /*return*/];
+                }
+            });
+        });
+    };
+    CHaser.prototype.check_first_str = function () {
+        if (this.response.toString()[0] == '0') {
+            console.log("Exit: the first str == 0");
+            process_1.exit();
+        }
+        else {
+            console.log("the first str == " + this.response.toString()[0]);
+        }
+    };
+    CHaser.prototype.make_value = function () {
+        this.value = Array.from(this.response.toString().substring(1, 10));
+        console.log("value : " + this.value);
+    };
+    // WALK
+    CHaser.prototype.walk_up = function () {
+        return __awaiter(this, void 0, void 0, function () {
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0: return [4 /*yield*/, this.action("wu\r\n")];
+                    case 1:
+                        _a.sent();
+                        return [2 /*return*/];
+                }
+            });
+        });
+    };
+    CHaser.prototype.walk_right = function () {
+        return __awaiter(this, void 0, void 0, function () {
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0: return [4 /*yield*/, this.action("wr\r\n")];
+                    case 1:
+                        _a.sent();
+                        return [2 /*return*/];
+                }
+            });
+        });
+    };
+    CHaser.prototype.walk_left = function () {
+        return __awaiter(this, void 0, void 0, function () {
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0: return [4 /*yield*/, this.action("wl\r\n")];
+                    case 1:
+                        _a.sent();
+                        return [2 /*return*/];
+                }
+            });
+        });
+    };
+    CHaser.prototype.walk_down = function () {
+        return __awaiter(this, void 0, void 0, function () {
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0: return [4 /*yield*/, this.action("wd\r\n")];
+                    case 1:
+                        _a.sent();
+                        return [2 /*return*/];
+                }
+            });
+        });
+    };
+    // LOOK
+    CHaser.prototype.look_up = function () {
+        return __awaiter(this, void 0, void 0, function () {
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0: return [4 /*yield*/, this.action("lu\r\n")];
+                    case 1:
+                        _a.sent();
+                        return [2 /*return*/];
+                }
+            });
+        });
+    };
+    CHaser.prototype.look_right = function () {
+        return __awaiter(this, void 0, void 0, function () {
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0: return [4 /*yield*/, this.action("lr\r\n")];
+                    case 1:
+                        _a.sent();
+                        return [2 /*return*/];
+                }
+            });
+        });
+    };
+    CHaser.prototype.look_left = function () {
+        return __awaiter(this, void 0, void 0, function () {
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0: return [4 /*yield*/, this.action("ll\r\n")];
+                    case 1:
+                        _a.sent();
+                        return [2 /*return*/];
+                }
+            });
+        });
+    };
+    CHaser.prototype.look_down = function () {
+        return __awaiter(this, void 0, void 0, function () {
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0: return [4 /*yield*/, this.action("ld\r\n")];
+                    case 1:
+                        _a.sent();
+                        return [2 /*return*/];
+                }
+            });
+        });
+    };
+    // SEARCH
+    CHaser.prototype.search_up = function () {
+        return __awaiter(this, void 0, void 0, function () {
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0: return [4 /*yield*/, this.action("su\r\n")];
+                    case 1:
+                        _a.sent();
+                        return [2 /*return*/];
+                }
+            });
+        });
+    };
+    CHaser.prototype.search_right = function () {
+        return __awaiter(this, void 0, void 0, function () {
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0: return [4 /*yield*/, this.action("sr\r\n")];
+                    case 1:
+                        _a.sent();
+                        return [2 /*return*/];
+                }
+            });
+        });
+    };
+    CHaser.prototype.search_left = function () {
+        return __awaiter(this, void 0, void 0, function () {
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0: return [4 /*yield*/, this.action("sl\r\n")];
+                    case 1:
+                        _a.sent();
+                        return [2 /*return*/];
+                }
+            });
+        });
+    };
+    CHaser.prototype.search_down = function () {
+        return __awaiter(this, void 0, void 0, function () {
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0: return [4 /*yield*/, this.action("sd\r\n")];
+                    case 1:
+                        _a.sent();
+                        return [2 /*return*/];
+                }
+            });
+        });
+    };
+    // PUT
+    CHaser.prototype.put_up = function () {
+        return __awaiter(this, void 0, void 0, function () {
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0: return [4 /*yield*/, this.action("pu\r\n")];
+                    case 1:
+                        _a.sent();
+                        return [2 /*return*/];
+                }
+            });
+        });
+    };
+    CHaser.prototype.put_right = function () {
+        return __awaiter(this, void 0, void 0, function () {
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0: return [4 /*yield*/, this.action("pr\r\n")];
+                    case 1:
+                        _a.sent();
+                        return [2 /*return*/];
+                }
+            });
+        });
+    };
+    CHaser.prototype.put_left = function () {
+        return __awaiter(this, void 0, void 0, function () {
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0: return [4 /*yield*/, this.action("pl\r\n")];
+                    case 1:
+                        _a.sent();
+                        return [2 /*return*/];
+                }
+            });
+        });
+    };
+    CHaser.prototype.put_down = function () {
+        return __awaiter(this, void 0, void 0, function () {
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0: return [4 /*yield*/, this.action("pd\r\n")];
+                    case 1:
+                        _a.sent();
+                        return [2 /*return*/];
+                }
+            });
+        });
+    };
+    CHaser.prototype.action = function (order) {
+        return __awaiter(this, void 0, void 0, function () {
+            var e_4;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0:
+                        _a.trys.push([0, 5, , 6]);
+                        return [4 /*yield*/, this.promiseSocket.writeAll(order)];
+                    case 1:
+                        _a.sent();
+                        return [4 /*yield*/, this.get_response()];
+                    case 2:
+                        _a.sent();
+                        return [4 /*yield*/, this.send_sharp()];
+                    case 3:
+                        _a.sent();
+                        return [4 /*yield*/, this.get_atMark()];
+                    case 4:
+                        _a.sent();
+                        return [3 /*break*/, 6];
+                    case 5:
+                        e_4 = _a.sent();
+                        console.error("error: action " + order + " : " + e_4);
+                        process_1.exit();
+                        return [3 /*break*/, 6];
+                    case 6: return [2 /*return*/];
+                }
+            });
+        });
+    };
+    return CHaser;
+}());
+function main() {
+    return __awaiter(this, void 0, void 0, function () {
+        var HOST, PORT, NAME, client, e_5;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    HOST = "127.0.0.1";
+                    PORT = 2009;
+                    NAME = "Your Name";
+                    client = new CHaser();
+                    _a.label = 1;
+                case 1:
+                    _a.trys.push([1, 20, , 21]);
+                    return [4 /*yield*/, client.socket(HOST, PORT, NAME)];
+                case 2:
+                    _a.sent();
+                    return [4 /*yield*/, client.get_atMark()];
+                case 3:
+                    _a.sent();
+                    _a.label = 4;
+                case 4:
+                    if (!true) return [3 /*break*/, 19];
+                    return [4 /*yield*/, client.get_ready()];
+                case 5:
+                    _a.sent();
+                    return [4 /*yield*/, client.search_left()];
+                case 6:
+                    _a.sent();
+                    return [4 /*yield*/, client.get_ready()];
+                case 7:
+                    _a.sent();
+                    return [4 /*yield*/, client.search_up()];
+                case 8:
+                    _a.sent();
+                    return [4 /*yield*/, client.get_ready()];
+                case 9:
+                    _a.sent();
+                    return [4 /*yield*/, client.search_down()];
+                case 10:
+                    _a.sent();
+                    return [4 /*yield*/, client.get_ready()];
+                case 11:
+                    _a.sent();
+                    return [4 /*yield*/, client.look_right()];
+                case 12:
+                    _a.sent();
+                    return [4 /*yield*/, client.get_ready()];
+                case 13:
+                    _a.sent();
+                    return [4 /*yield*/, client.look_left()];
+                case 14:
+                    _a.sent();
+                    return [4 /*yield*/, client.get_ready()];
+                case 15:
+                    _a.sent();
+                    return [4 /*yield*/, client.look_up()];
+                case 16:
+                    _a.sent();
+                    return [4 /*yield*/, client.get_ready()];
+                case 17:
+                    _a.sent();
+                    return [4 /*yield*/, client.look_down()];
+                case 18:
+                    _a.sent();
+                    return [3 /*break*/, 4];
+                case 19: return [3 /*break*/, 21];
+                case 20:
+                    e_5 = _a.sent();
+                    console.error("error in main: " + e_5);
+                    process_1.exit();
+                    return [3 /*break*/, 21];
+                case 21: return [2 /*return*/];
+            }
+        });
+    });
+}
+main();
+//# sourceMappingURL=CHaser.js.map
